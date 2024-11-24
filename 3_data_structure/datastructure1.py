69bd19d7f55a3ffa940de14cd4e76fbe5b71f291
sunilsala88 <sunilsala88@gmail.com>
2024-11-24 15:13:58 +0530

    nov-24-2

--- 3_data_structure/datastructure1.py
+++ 3_data_structure/datastructure1.py
@@ -6,7 +6,32 @@ l1.append(98)
 l1.append(98)
 
 l1.remove(33)
-l1.pop(0)
+a=l1.pop(0)
+print(a)
 print(l1)
-l1.insert(0,'hello')
+l1.insert(0,65)
+
+
+i=l1.index(65)
+print(i)
 print(l1)
+a1=[1,2,3]
+s2=[5,6,7]
+a1.extend(s2)
+print(a1)
+
+stocks=['amzn','tsla']
+stocks.insert(10,88)
+print(stocks)
+
+s1='amzn--tsla--goog--nifty'
+w=s1.split(',')
+print(w)
+
+k='@'.join(w)
+print(k)
+
+a='  ab c  '
+print(a.strip())
+
+s1='amzn--tsla--goog--nifty'
