ba4789da00c2d950fb1dad4f00ed4463d46379cd
sunilsala88 <sunilsala88@gmail.com>
2024-12-24 09:46:40 +0530

    dec 22

---
+++ 8_module/module3.py
@@ -0,0 +1,31 @@
+
+
+#numpy
+#pandas
+
+#install
+#pip install pkg_name
+
+#uninstall
+#pip uninstall pkg_name
+
+
+#update
+#pip install --upgrade pkg_name
+
+#list all installed packages
+#pip list or pip freeze
+
+#downgrade to a specific version
+#pip install pkg_name==version
+
+
+import numpy
+import numpy as np
+import file1
+import file1 as q1
+from file1 import a,b
+
+print(a,b)
+
+
