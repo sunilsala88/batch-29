4ef5afec739bb41c9cc18512e646d328a3d6c699
sunilsala88 <sunilsala88@gmail.com>
2024-12-30 12:56:03 +0530

    dec-29

---
+++ 10_data_analysis/pandas1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+x=np.arange(25,50).reshape(5,5)
+print(x)
+df1=pd.DataFrame(x)
+print(df1)
+
+df2=pd.read_csv(r'/Users/algotrading2024/batch 29/10_data_analysis/sp500.csv')
+print(df2)
+
+#mac /
+#wind \
