828f978fd865d34f6e8761cf426675f05b4909f9
sunilsala88 <sunilsala88@gmail.com>
2025-01-04 16:33:45 +0530

    jan-4

--- 10_data_analysis/pandas1.py
+++ 10_data_analysis/pandas1.py
@@ -1,12 +1,23 @@
+# import pandas as pd
+# import numpy as np
+# x=np.arange(25,50).reshape(5,5)
+# print(x)
+# df1=pd.DataFrame(x)
+# print(df1)
+
+
+# import pandas as pd
+# stocks=pd.read_csv(r'stock_value.csv')
+# print(stocks)
+
 import pandas as pd
-import numpy as np
-x=np.arange(25,50).reshape(5,5)
-print(x)
-df1=pd.DataFrame(x)
-print(df1)
+#pip install lxml
+# d=pd.read_html('https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average')
+# print(d)
 
-df2=pd.read_csv(r'/Users/algotrading2024/batch 29/10_data_analysis/sp500.csv')
-print(df2)
 
-#mac /
-#wind \
+#pip install openpyxl
+# a=pd.read_excel('/Users/algotrading2024/batch 29/Book1.xlsx')
+# print(a)
+
+# print()
