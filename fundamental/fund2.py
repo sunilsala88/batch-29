2e201d182946f700f78c6faed84b7c69eb6cf1f2
sunilsala88 <sunilsala88@gmail.com>
2025-01-20 10:45:16 +0530

    jan-18

--- fundamental/fund2.py
+++ fundamental/fund2.py
@@ -8,15 +8,28 @@ print(stock)
 stock_fundament = stock.ticker_fundament()
 print(stock_fundament)
 
-import pandas as pd
-df=pd.Series(stock_fundament).to_csv('data.csv')
+# import pandas as pd
+# df=pd.Series(stock_fundament).to_csv('data.csv')
 
 
 from finvizfinance.screener.overview import Overview
 
 foverview = Overview()
-filters_dict = {'Index':'NASDAQ 100','Market Cap.':'Mega ($200bln and more)'}
+filters_dict = {'Index':'NASDAQ 100','Market Cap.':'Mega ($200bln and more)','Current Volume':'Over 20M'}
 foverview.set_filter(filters_dict=filters_dict)
 df = foverview.screener_view()
 df.head()
 print(df)
+
+list_of_stocks=df['Ticker'].to_list()
+dict_of_pe={}
+
+for stock in list_of_stocks:
+    stock = finvizfinance(stock)
+    stock_fundament = stock.ticker_fundament()
+
+    dict_of_pe[stock_fundament['Company']]=stock_fundament['P/E']
+    
+print(dict_of_pe.values())
+print(len(dict_of_pe.values()))
+print(max(dict_of_pe.values()))  
