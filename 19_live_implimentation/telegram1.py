db23a2b2f57e02991d9a84a94c1e859cb21f627e
sunilsala88 <sunilsala88@gmail.com>
2025-03-23 15:09:43 +0530

    mar23-4

--- 19_live_implimentation/telegram1.py
+++ 19_live_implimentation/telegram1.py
@@ -1,8 +1,8 @@
 
 
 import requests
-TOKEN = '7956287339:AAH7mdFBkmsY0ngU2No5sCAWf6-n-bBwghg'
-ids = '5563890177'
+TOKEN = ''
+ids = ''
 
 message='oder placed on goog at price 150'
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ids}&parse_mode=Markdown&text={message}"
